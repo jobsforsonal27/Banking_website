21c37dd0c142753ceecc80f8a8d24b6ff7a86bd3
Sonal <jobsforsonal27>
2026-01-19 21:23:23 -0500

    host added

--- bankproject/settings.py
+++ bankproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)pf-x7jwto5lq*kp@-76+8c4%4o#qg9pq0x824s82jj6t7!3nx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','34.201.103.112']
 
 
 # Application definition
