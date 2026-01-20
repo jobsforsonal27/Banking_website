d1a9b2cb20cd5ec2ddf9e1730b82487e8efdec64
Sonal <jobsforsonal27>
2026-01-19 20:32:59 -0500

    username change

--- bankproject/settings.py
+++ bankproject/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'bankproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres-db',
+        'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'Anupama123',
         'HOST': 'postgres-db.ckl8ski4chbc.us-east-1.rds.amazonaws.com',
